117d7ea8369ec5320039a822d7c3d413c1956566
andrewevans0102 <andrewevans0102@gmail.com>
2021-06-06 08:33:36 -0400

    first commit

---
+++ apps/first-project/src/app/app.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+
+interface Todo {
+  title: string;
+}
+
+const App = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    fetch('/api/todos')
+      .then((_) => _.json())
+      .then(setTodos);
+  }, []);
+
+  function addTodo() {
+    fetch('/api/addTodo', {
+      method: 'POST',
+      body: '',
+    })
+      .then((_) => _.json())
+      .then((newTodo) => {
+        setTodos([...todos, newTodo]);
+      });
+  }
+
+  return (
+    <>
+      <h1>Todos</h1>
+      <ul>
+        {todos.map((t) => (
+          <li className={'todo'}>{t.title}</li>
+        ))}
+      </ul>
+      <button id={'add-todo'} onClick={addTodo}>
+        Add Todo
+      </button>
+    </>
+  );
+};
+
+export default App;
